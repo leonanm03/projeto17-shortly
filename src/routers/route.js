22639b40a5b54f9da6a2bea61477f9e563c62d17
Leonan <leonanm04@gmail.com>
2023-02-21 14:24:28 -0300

    deploy config

--- src/routers/route.js
+++ src/routers/route.js
@@ -1,1 +1,8 @@
-// routers
+import { Router } from "express";
+import { getTeste } from "../controllers/controller.js";
+
+const router = Router();
+
+router.get("/teste", getTeste);
+
+export default router;
